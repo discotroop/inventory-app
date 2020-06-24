4d165e1bd4bba99e61b8324464cd44f46fe8572d
John Mark Goodson <john.mark.hat@gmail.com>
2020-06-24 15:44:02 -0400

    produce type create form now creates now produce types

--- controllers/produceTypeController.js
+++ controllers/produceTypeController.js
@@ -2,6 +2,11 @@ let ProduceType = require('../models/produceType');
 let async = require('async');
 let Item = require('../models/item');
 
+// Validator for create and update.
+const validator = require('express-validator')
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 
 
 // Display all produce types on GET
@@ -40,13 +45,50 @@ exports.producetype_create_get = function (req, res, next) {
     res.render("produce_type_create", {title: "create new produce type"});
 };
 
-
-
 // Handle create on POST
-exports.producetype_create_post = function (req, res, next) {
-    res.render("index", {title: "tbd"});
-};
+exports.producetype_create_post = [
+    // require name and trim spaces on end
+    validator.body('name', 'Produce name required').trim().isLength({ min: 1}),
+    // clean up name
+    validator.sanitizeBody('name').escape(),
 
+    // Process validated request.
+    (req, res, next) => {
+        // Check for validation errors
+        const errors = validator.validationResult(req);
+        // Create new produce type object
+        let producetype = new ProduceType(
+            {
+                name: req.body.name,
+                description: req.body.description,
+            }
+        );
+        // check for errors
+        if (!errors.isEmpty()) {
+            // if errors return create form with errors
+            res.render('produce_type_create', { title: 'create new produce type', produce_type: producetype, erros: erros.array()});
+            return
+        }
+        else {
+            // Data is valid
+            // Check for redundancies in produce type
+            ProduceType.findOne({ 'name': req.body.name })
+            .exec( function(err, found_type) {
+                if(err) {return next(err);}
+                if (found_type) {
+                    // redirect to redundant url
+                    res.redirect(found_type.url)
+                }
+                else {
+                    producetype.save(function (err) {
+                        if (err) { return next(err); }
+                        res.redirect(producetype.url)
+                    });
+                }
+            });
+        }
+    }
+]
 // Display delete on GET
 exports.producetype_delete_get = function (req, res, next) {
     res.render("index", {title: "tbd"});
@@ -58,10 +100,24 @@ exports.producetype_delete_post = function (req, res, next) {
 
 // Display update screen on GET
 exports.producetype_update_get = function (req, res, next) {
-    res.render("index", {title: "tbd"});
+    async.parallel({
+        produceType: function (callback) {
+            ProduceType.findById(req.params.id)
+            .exec(callback);
+        },
+    }, function (err, results) {
+        if(err) { return next(err)}
+        if(results.produceType == null) {
+            let err = new Error("produce type not found")
+            err.status = 404;
+            return next(err);
+        }
+        res.render("produce_type_create", {title: results.produceType.name, produce_type: results.produceType});
+
+    });
 };
 // Handle update request on POST 
 exports.producetype_update_post = function (req, res, next) {
-    res.render("index", {title: "tbd"});
+    res.render("index", {title: results.produceType.name, produce_type: results.produceType});
 };
 
